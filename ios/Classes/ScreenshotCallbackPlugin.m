a1110ce6209241cb87e9d2fa6152e691d7e1d1a2
Nebi Sarigul <n.sarigul1@gmail.com>
2026-06-08 03:36:45 +0300

    Remove iOS ScreenshotCallbackPlugin implementation

--- ios/Classes/ScreenshotCallbackPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "ScreenshotCallbackPlugin.h"
-#import <screenshot_callback_fix/screenshot_callback_fix-Swift.h>
-
-@implementation ScreenshotCallbackPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftScreenshotCallbackPlugin registerWithRegistrar:registrar];
-}
-@end
